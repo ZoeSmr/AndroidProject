d35a0867b03e7fb6b8050e223aeb95aa535f95e4
ZoeSmr <73388676+ZoeSmr@users.noreply.github.com>
2021-09-12 20:52:39 +0800

    Update MainActivity.java

--- DialActicity/app/src/main/java/com/example/dialacticity/MainActivity.java
+++ DialActicity/app/src/main/java/com/example/dialacticity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dialacticity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        call("18864512284");
+    }
+
+    private void call(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
